2bcd9b2aff5efa24bd049af2586c74b589b58441
Sreenivas <sreenivasmuthyalapati@gmail.com>
2024-01-09 22:35:14 +0530

    testt

--- src/test/java/test/ApiRequest.java
+++ src/test/java/test/ApiRequest.java
@@ -11,19 +11,15 @@ import java.io.InputStreamReader;
 
 public class ApiRequest {
     public static void main(String[] args) throws IOException {
-        // Create an HTTP client
+
         HttpClient httpClient = HttpClients.createDefault();
 
-        // Define the API endpoint URL
         String apiUrl = "https://library-api.postmanlabs.com/books";
 
-        // Create an HTTP GET request
         HttpGet httpGet = new HttpGet(apiUrl);
 
-        // Execute the request and get the response
         HttpResponse response = httpClient.execute(httpGet);
 
-        // Read and print the API response
         BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
         String line;
         StringBuilder responseContent = new StringBuilder();
@@ -32,13 +28,10 @@ public class ApiRequest {
             responseContent.append(line);
         }
 
-        // Close the reader
         reader.close();
 
-        // Print the API response
         System.out.println("API Response: " + responseContent.toString());
 
-        // You can now parse and process the API response as needed
     }
 }
 
